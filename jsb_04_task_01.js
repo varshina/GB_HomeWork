df4f3ffda5fb929e273916b29c85270c98c49b45
vershina <vershinina.natalia.23@gmail.com>
2023-07-24 20:29:09 +0300

    Добавлена домашняя работа по курсу 'Основы JavaScript' : Урок 4 : Задания 1,2,3,4

---
+++ jsb_04_task_01.js
@@ -0,0 +1,11 @@
+const num1 = prompt('Введите чтсло num1');
+
+if (num1 == 1) {
+    console.log("Переменная num1 равна 1");
+} else if (num1 < 1){
+    console.log('Переменная num1  меньше 1');
+} else {
+    console.log('Переменная num1 больше 1');
+}
+
+
