f56866120bb33a140089b0f2e7d079a26b6af1ff
vershina <vershinina.natalia.23@gmail.com>
2023-07-25 12:49:48 +0300

    Внесены незначительные корректировки

--- Lesson_06_jsb/jsb_06_task_01.js
+++ Lesson_06_jsb/jsb_06_task_01.js
@@ -11,20 +11,3 @@ const cubing2 = functCubing(3);
 
 console.log(cubing1 + cubing2);
 
-
-
-/
-
-
- /**Задание 3
-Пользователь с клавиатуры вводит 3 числа, необходимо создать функцию, которая определяет максимальное значение среди этих чисел 
-
-function maxNumber(num1, num2, num3) {
-	if (num1 > num2) {
-		console.log(`${num2} - минимальное число`);
-	} else {
-		console.log(`${num1} - минимальное число`);
-	}
-}
-
-minMaxFn(1, 10);*/
